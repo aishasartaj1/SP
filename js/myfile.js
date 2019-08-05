45f521f68595fb9c2a6987e8e9cb9ad12c45839f
aishasartaj1 <49358300+aishasartaj1@users.noreply.github.com>
2019-08-05 19:34:11 +0530

    transfer of js script

--- js/myfile.js
+++ js/myfile.js
@@ -3,3 +3,9 @@ function myFunction() {
   alert("Power is gained by sharing knowledge,not hoarding it!!");}
 
 
+
+function myFunction1() {
+  document.getElementById("demo").innerHTML = "MY FIRST EVENT IN IET!!";
+}
+var d = new Date();
+document.getElementById("demo").innerHTML = d.toUTCString();

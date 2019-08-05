b44db2709ac30b951c3f9827ec821bc7aeea5852
aishasartaj1 <49358300+aishasartaj1@users.noreply.github.com>
2019-08-05 19:43:04 +0530

    change in js1

--- js/myfile.js
+++ js/myfile.js
@@ -2,10 +2,8 @@
 function myFunction() {
   alert("Power is gained by sharing knowledge,not hoarding it!!");}
 
-
-
 function myFunction1() {
   document.getElementById("demo").innerHTML = "MY FIRST EVENT IN IET!!";
 }
 var d = new Date();
-document.getElementById("demo").innerHTML = d.toUTCString();
+document.getElementById("demo1").innerHTML = d.toUTCString();
